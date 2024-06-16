4dafe0e9b4cd1391bfb40691801d5ded368e4d95
= <=>
2024-06-17 03:03:43 +0530

    added analytics

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Layout from "./Components/layout";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}><Layout>{children}</Layout></body>
+      <body className={inter.className}><Layout><Analytics/>{children}</Layout></body>
     </html>
   );
 }
